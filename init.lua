95c4d0e6bea9918f0cbfe12d4ab2d4e7a77736bd
Wassim KABALAN <wastondev@gmail.com>
2024-07-22 00:04:29 +0200

    update

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 
 -- bootstrap lazy and all plugins
@@ -27,6 +28,9 @@ require("lazy").setup({
   { import = "plugins" },
 }, lazy_config)
 
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
 
 require "nvchad.autocmds"
 
