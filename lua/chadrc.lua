a55df87a19e973a77a8c2b61288dc7b419ddcf8b
Wassim KABALAN <wastondev@gmail.com>
2024-07-22 00:40:18 +0200

    refresh repo

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,20 +1,16 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+
 ---@type ChadrcConfig
 local M = {}
 
--- Path to overriding theme and highlights files
-local highlights = require "highlights"
-
 M.ui = {
-  theme = "catppuccin",
-  theme_toggle = { "catppuccin", "one_light" },
+	theme = "onedark",
 
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
 }
-
--- M.plugins = "plugins"
-
--- check core.mappings for table structure
--- M.mappings = require "mappings"
 
 return M
